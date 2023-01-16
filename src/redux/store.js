3287d7ad6c47a434e0f6cbf82cefd70fb334a6dc
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-01-16 11:45:59 -0500

    configurando store

---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+
+import pokeReducer from './pokeDucks';
+
+const rootReducer = combineReducers({
+    pokemones: pokeReducer
+})
+
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
+
+export default function generateStore(){
+    const store = createStore( rootReducer, composeEnhancers( applyMiddleware( thunk )))
+    return store;
+
+}
